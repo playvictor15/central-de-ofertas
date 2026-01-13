e5fe237b02ee914023eeb74650890845247cf455
playvictor15 <127317443+playvictor15@users.noreply.github.com>
2026-01-13 11:16:19 -0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,20 +1,28 @@
 function salvarProduto() {
   const loja = document.getElementById('loja').value;
   const nome = document.getElementById('nome').value.trim();
+  const imagem = document.getElementById('imagem').value.trim();
   const link = document.getElementById('link').value.trim();
 
   if (!nome || !link) {
-    alert('Preencha todos os campos corretamente.');
+    alert('Nome e link são obrigatórios.');
     return;
   }
 
   const produtos = JSON.parse(localStorage.getItem(loja)) || [];
-  produtos.push({ nome, link });
+
+  produtos.push({
+    nome,
+    imagem,
+    link
+  });
+
   localStorage.setItem(loja, JSON.stringify(produtos));
 
   alert('Produto salvo com sucesso.');
 
   document.getElementById('nome').value = '';
+  document.getElementById('imagem').value = '';
   document.getElementById('link').value = '';
 }
 
@@ -37,14 +45,18 @@ function carregarProdutos(loja, containerId) {
 
     card.innerHTML = `
   <div class="produto-card">
+    ${
+      produto.imagem
+        ? `<img src="${produto.imagem}" alt="${produto.nome}">`
+        : ''
+    }
     <h3>${produto.nome}</h3>
     <a href="${produto.link}" target="_blank" rel="noopener" class="btn ${loja}">
       Comprar agora
     </a>
   </div>
 `;
-
-
+    
     container.appendChild(card);
   });
 }
